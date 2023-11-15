029bf58e66b7b849e96a487c82fe31d059d76b5d
insung5189 <insung5189@gmail.com>
2023-11-15 20:16:54 +0900

    2-10 질문 상세

--- src/main/java/com/ll/sbb_1/question/QuestionService.java
+++ src/main/java/com/ll/sbb_1/question/QuestionService.java
@@ -2,8 +2,10 @@ package com.ll.sbb_1.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.ll.sbb_1.DataNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +15,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
