e4572d138c901fef988370635d97148dc88992ba
insung5189 <insung5189@gmail.com>
2023-11-16 00:41:52 +0900

    3-12 앵커

--- src/main/java/com/ll/sbb_1/answer/AnswerService.java
+++ src/main/java/com/ll/sbb_1/answer/AnswerService.java
@@ -15,7 +15,7 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = Answer.builder()
                 .content(content)
                 .createDate(LocalDateTime.now())
@@ -23,6 +23,7 @@ public class AnswerService {
                 .author(author)
                 .build();
         this.answerRepository.save(answer);
+        return answer;
     }
     public Answer getAnswer(Integer id) {
         Optional<Answer> answer = this.answerRepository.findById(id);
